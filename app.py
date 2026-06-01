c135a1386739732be5ed7290bbfb591f2955900b
Daniela <dani.a.menezes@outlook.com>
2026-05-31 23:11:55 -0300

    feat:Only grades greater than zero or less than or equal to 5 are accepted

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante("praça", "Gourmet")
-restaurante_praca.receber_avaliacao("joão", 9)
-restaurante_praca.receber_avaliacao("dani", 10)
-restaurante_praca.receber_avaliacao("gi", 7)
+# restaurante_praca.receber_avaliacao("joão", 9)
+# restaurante_praca.receber_avaliacao("dani", 10)
+# restaurante_praca.receber_avaliacao("gi", 7)
 
 
 def main():
