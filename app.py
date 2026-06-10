2659195496a1898da7d1b9f6da016faf523bec97
danianjosm <dani.a.menezes@outlook.com>
2026-06-10 18:07:16 -0300

    feat: add display menu functionality

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante("praça", "Gourmet")
 bebida_suco = Bebida("Suco de melancia", 5.0, "grande")
 prato_pao = Prato("pão", 2.00, "O melhor pão da cidade")
+restaurante_praca.adicionar_nocardapio(bebida_suco)
+restaurante_praca.adicionar_nocardapio(prato_pao)
 
 
 def main():
-    print(bebida_suco)
-    print(prato_pao)
+    restaurante_praca.exibir_cardapio
 
 
 
 if __name__ == "__main__":
     main()
+
+
--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,4 +1,5 @@
 from modelos.cardapio.item_cardapio import ItemCardapio
+
 class Prato(ItemCardapio): # essa classe prato vai herdar metodo atributos de outra classe 
     def __init__(self, nome, preco, descricao):
         super().__init__(nome,preco)
